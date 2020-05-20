01c22965647ae0b366456fbece4ab0b5626ee1d6
zeroxu <qaqaqa020@gmail.com>
2020-05-20 12:46:50 +0800

    2020/05/20

---
+++ Assets/Script/changeLogin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeLogin : MonoBehaviour
+{
+   public void OnStartGame(string ScneneName){
+       SceneManager.LoadScene("login");
+   }
+}
