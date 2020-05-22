f2128caf167d162f85183818e6f9d5af3017b96f
zeroxu <qaqaqa020@gmail.com>
2020-05-22 17:10:58 +0800

    2020/05/22

---
+++ Assets/Script/changeScore.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScore : MonoBehaviour
+{
+    public void OnStartGame(string ScneneName){
+       SceneManager.LoadScene("score");
+    }
+}
